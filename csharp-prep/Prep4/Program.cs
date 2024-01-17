de51fae344b86ba1376e7cf5888f9406284a4bfe
RafaelJim92 <rafael.jimeneza@outlook.com>
2024-01-16 23:02:39 -0700

    prep 4 some debugging ><> almost working

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber;
+
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out userNumber))
+            {
+                if (userNumber != 0)
+                {
+                    numbers.Add(userNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        } while (userNumber != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
