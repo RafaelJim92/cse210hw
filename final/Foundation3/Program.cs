a57dd1844ee6c709d816808b13eb50b1be22e73d
RafaelJim92 <rafael.jimeneza@outlook.com>
2024-02-19 19:08:15 -0500

    worked on found 2-4 fixing code

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -3,6 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
         Console.WriteLine("Hello Foundation3 World!");
     }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation4 World!");
